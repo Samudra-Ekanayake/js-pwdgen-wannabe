66e26572b6d32507e377c17bdc9431db6e33d8e7
Samudra-Ekanayake <samudra2724@gmail.com>
2024-04-16 17:44:47 +0200

    esercizio completo

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const name =prompt ("inserisci il tuo nome");
 const surname = prompt ("inserisci il tuo cognome");
 const favouritecolor =prompt ("inserisci il tuo colore preferito");
-const password = "name" + "surname" + "favouritecolor" + "#24";
+const password = "Mario" + "Rossi" + "Verde" + "#24";
 alert(password)
 
